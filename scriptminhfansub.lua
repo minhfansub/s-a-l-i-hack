1f2ecc74ae4e36204078f8f962d7a10e6a664eab
Minhfansub <phambinhminhhk@gmail.com>
2025-03-28 19:28:28 +0700

    Update s l scriptminhfansub.lua

--- scriptminhfansub.lua
+++ scriptminhfansub.lua
@@ -311,7 +311,7 @@ v5:Destroy();
 --         pcall(v422);
 --     end
 -- end);
-local v14 = loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
+local v14 =
 local v15 = v14:CreateWindow({
     Title = "minhfansub roblox Hub",
     SubTitle = "Blox Fruit",
